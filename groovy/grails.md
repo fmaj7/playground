5c3b05c4e54d495fbd1d9b70b5d28dabb19c9fab
fmaj7 <fmaj7@live.com>
2015-01-04 19:33:45 -0800

    Adding old groovy thingy

---
+++ groovy/grails.md
@@ -0,0 +1,5 @@
+== Misc
+
+* findOrSaveWhere doesn't work with multiple parameters
+
+* Grails upgrade doesn't work. http://grails.1312388.n4.nabble.com/Should-I-and-how-do-I-permanently-get-rid-of-the-default-Grails-web-app-files-td4631624.html#a4631664
---
+++ groovy/hello-world.groovy
@@ -0,0 +1,32 @@
+println "Hello, World!"
+
+println 123+45*67
+
+// Number
+x = 1
+println x
+
+// Number?
+x = -3.1499392
+println x
+
+// Java Date
+x = new java.util.Date()
+println x
+
+// True/False
+x = false
+println x
+
+// String
+x = "Hi"
+println x
+
+// List
+myList = [1776, -1, 33, 99, 0, 928734928763]
+println myList
+println myList[0]
+println myList.size()
+
+// Maps
+scores = [ "Brett":100, "Pete":"Did not finish", "Andrew":86.87934 ]
